b5315ba84f8f353ed7021496d8db361b64cb2f55
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-05-25 18:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let ans = "";
+  while(n != 0){
+	  ans = (n%2) + ans;
+	  n = praseInt(n/2);
+  }
+	return ans;
 }
 
 module.exports = threeSum;
